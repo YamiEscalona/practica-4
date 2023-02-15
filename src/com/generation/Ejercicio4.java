feff8dcf37acbef9e05cca210bd24911068baab7
Yami Escalona <yamescalord@gmail.com>
2023-02-15 17:56:05 -0600

    codigo de otros 4

---
+++ src/com/generation/Ejercicio4.java
@@ -0,0 +1,46 @@
+package com.generation;
+
+import java.util.Scanner; //Scanner no estaba importado
+
+//Cambie el combre de la clase por el nombre del documento
+public class Ejercicio4 {
+	// No tenia la linea de main
+	public static void main(String[] args) {
+
+	Scanner s = new Scanner(System.in); //Se agrego el System.in
+
+	System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+	String j1 = s.nextLine();
+
+	//Se le cambio el 1 por el 2
+	System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+	Scanner s2 = new Scanner(System.in);//Se agrego el System.in
+	String j2 = s.nextLine();
+
+	//Habia un ) demas, y se le quito
+	if(j1==j2) {
+		System.out.println("Empate");
+	} else {
+		int g = 2;
+		switch(j1) {
+		case "piedra":
+			if (j2 == "tijeras") {
+				g = 1;
+				}
+			break; // Faltaba break
+        case "papel":
+        	if (j2 == "piedra") {
+        		g = 1;
+        	} // Faltaba un }
+        	break; // Faltaba break
+        case "tijera":
+          if (j2.equals("papel")) {
+        	  g = 1;
+          }
+          break;
+        default:
+      }
+      System.out.println("Gana el jugador " + g);
+      }
+	}
+	} // Faltaba un }
